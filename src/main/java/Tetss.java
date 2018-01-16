479f686f53a10f7d9ad26ee51b5cb465c555c0fb
pawel <pawel.gondek@gmail.com>
2018-01-16 22:04:08 +0100

    [pawel] first lesson - intro to getter and setter

--- src/main/java/Tetss.java
+++ src/main/java/Tetss.java
@@ -3,6 +3,14 @@
  */
 public class Tetss {
 
+    // pola ZAWSZE musza byc prywatne
     private String name;
     private String nazwisko;
+
+
+    //zeby zdoby wartosc z pola 'name' potrzebujemy stworzyc specjalna metode - tzw. getter
+
+    public String getName() {
+        return name;
+    }
 }
