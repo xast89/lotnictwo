dae5c3e521818d9304d297f23b7cd47f8e3bd9a8
Jacek Mroczek <jacko.mds@interia.pl>
2018-01-20 21:47:40 +0100

    Final version. But I don't know why I can't do system.out.println?

--- src/main/java/preLesson/PreLessonMain.java
+++ src/main/java/preLesson/PreLessonMain.java
@@ -12,24 +12,32 @@ public class PreLessonMain {
         // first we need to create new instance of Man class:
         Man firstObject = new Man("Pawel", 29);
         Man secondObject = new Man("Krzysiek", 55);
-        Man thirdObject = new Man( "Jacek",  27);
+        Man thirdObject = new Man("Jacek", 27);
         // display object's name
 
-        System.out.println("FirstObject name is: " + firstObject.getName ());
+        System.out.println("FirstObject name is: " + firstObject.getName());
         System.out.println("SecondObject name is: " + secondObject.getName());
         System.out.println("thirdObject name is: " + thirdObject.getName());
 
-        System.out.println ("FirstObject age is: " +firstObject.getAge());
-        System.out.println("secondObject age is: " +secondObject.getAge());
-        System.out.println("thirdObject age is: " +thirdObject.getAge());
-        
+        System.out.println("FirstObject age is: " + firstObject.getAge());
+        System.out.println("secondObject age is: " + secondObject.getAge());
+        System.out.println("thirdObject age is: " + thirdObject.getAge());
+
         //This is constructor with 4 params
         Woman firstWoman = new Woman("Kasia", 33, 1.98, true);
+        Woman secondWoman = new Woman("Beata", 14, 1.59,true);
+        Woman thirdWoman = new Woman("Agata", 18,  true);
+        Woman fourthWoman = new Woman(18, 170, true);
+        Woman fifhtWoman = new Woman();
+
+    System.out.println("firstWoman name is" + firstWoman.getName()+ firstWoman.getAge()+ firstWoman.getGrowth()+ firstWoman.isAdult());
     }
 
-        
 
-    //TODO: please create class Woman with 4 fields: int, double,String, boolean (for example: int age, String name... )
+
+//System.out.println ("firstWoman name is" + firstWoman.getName(),firstWoman.getAge(),firstWoman.getGrowth(), firstWoman.isAdult());
+
+//TODO: please create class Woman with 4 fields: int, double,String, boolean (for example: int age, String name... )
     //TODO: create 4 constructors: with no param, with one param, with two params, with three params and one with all 4 params
     //TODO: create getters for all 4 fields
 
--- src/main/java/preLesson/Woman.java
+++ src/main/java/preLesson/Woman.java
@@ -12,6 +12,32 @@ public class Woman {
         this.growth = growth;
         this.adult = adult;
     }
+    public Woman (String name, int age,boolean adult) {
+        this.name = name;
+        this.age = age;
+        this.growth = growth;
+        this.adult = adult;
+    }
+    public Woman (String name, double growth,boolean adult){
+        this.name = name;
+        this.growth = growth;
+        this.adult = adult;
+    }
+    public Woman (int age,double growth,boolean adult){
+        this.age = age;
+        this.growth = growth;
+        this.adult = adult;
+    }
+
+
+    public Woman (String name, int age, double growth) {
+        this.name = name;
+        this.age = age;
+        this.growth = growth;
+
+    }
+    public Woman (){}
+
       public String getName(){return name;}
 
     public int getAge() {
