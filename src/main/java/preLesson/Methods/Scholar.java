f82cd3f7c3bde571e70b2c433f0d9fa8fd34a30e
pawel <pawel.gondek@gmail.com>
2018-01-21 21:01:13 +0100

    [pawel] add next task: methods

---
+++ src/main/java/preLesson/Methods/Scholar.java
@@ -0,0 +1,12 @@
+package preLesson.Methods;
+
+/**
+ * Created by pawel on 21.01.18.
+ */
+public class Scholar {
+
+    public double adding(double a, double b)
+    {
+        return a + b;
+    }
+}
---
+++ src/main/java/preLesson/Methods/ScholarMain.java
@@ -0,0 +1,17 @@
+package preLesson.Methods;
+
+/**
+ * Created by pawel on 21.01.18.
+ */
+public class ScholarMain {
+
+
+    public static void main(String[] args) {
+
+        Scholar gandalf = new Scholar();
+
+        double result = gandalf.adding(6.88, 1.62);
+
+        System.out.println("Our adding result is " + result);
+    }
+}
